c051bdf78775a983d686e9cd5936fa5d303cdd4a
Alex Day <alexanderday@email.arizona.edu>
2024-01-17 21:25:04 -0700

    Fix markdown changes

--- main.py
+++ main.py
@@ -62,14 +62,13 @@ async def concerts(ctx, subcommand, user_name=None, json_file_name=None):
             try:
                 # For every json file linked to the user, we send them every event's artist, date, and venue
                 for file_name in user_to_json[user_name]:
-                    message = f"## Upcoming concerts for user @{user_name} from file {file_name}:<<<\n"
+                    message = f"## Upcoming concerts for user @{user_name} from file {file_name}:\n"
                     with open(file_name, 'r') as file:
                         data = json.load(file)
                         # Process and send concert data
                         for artist in data["artists"]:
                             for event in data["artists"][artist]["events"]:
-                                message += f"**{artist}**: *{event['datetime_utc']}* in {event['venue']['city']} at {event['venue']['name']}\n"
-                    message += ">>>\n"
+                                message += f"**{artist}**: \n >>> *{event['datetime_utc']}* in {event['venue']['city']} at {event['venue']['name']}\n"
                     await ctx.send(message)
             except FileNotFoundError:
                 await ctx.send("Error: JSON file not found.")
